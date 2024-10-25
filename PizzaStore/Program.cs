eb826cd09a6ddeba77b70e0a78c703dbed48f55d
shuc0019 <115073428+shuc0019@users.noreply.github.com>
2024-10-25 22:46:46 +0200

    Update Program.cs

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -5,18 +5,18 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // Create 3 pizza objects
+        // pizza objects
         Pizza margarita = new Pizza("Margarita", "Cheese", 45);
         Pizza saladPizza = new Pizza("Salad Pizza", "Salad, Kebab, Dressing", 65);
         Pizza pepperoni = new Pizza("Pepperoni", "Pepperoni, Cheese", 55);
 
-        // Create a customer object
+        // customer objects
         Customer customer1 = new Customer(1, "Alisa", 80706022);
         Customer customer2 = new Customer(1, "lisa", 40706022);
         Customer customer3 = new Customer(1, "Alisson", 20706022);
 
 
-        // Create Order objects, each with a different pizza
+        // Order objects each with a different pizza
         Order order1 = new Order(1, customer1, margarita);
         Order order2 = new Order(2, customer2, saladPizza);
         Order order3 = new Order(3, customer3, pepperoni);
